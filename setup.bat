09847cbc42060a6f67b8663bc8ce9ce6beb2da98
Chris Maughan <mornymorny@gmail.com>
2022-02-20 10:52:20 +0000

    Remove

--- setup.bat
+++
@@ -1,8 +0,0 @@
-REM Haskell:
-REM https://blog.jez.io/haskell-development-with-neovim/
-
-setx __COMPAT_LAYER=
-stack install hindent
-stack install stylish-haskell
-stack install hlint
-stack build ghc-mod
